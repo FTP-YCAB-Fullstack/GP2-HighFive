fc2a5764d6be6b460dd932a45ee3fde2e3cd7fb4
AlimMakruf <alimm.makruf@gmail.com>
2021-09-04 23:45:01 +0700

    Homepage styling non component

--- HighTrivia/src/pages/Question.js
+++ HighTrivia/src/pages/Question.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Question() {
-  return <h1>Question</h1>;
+  return (
+    <>
+      <nav className="flex justify-between bg-red-700 p-3 absolute top-0 z-50 w-full">
+        <span>MAWAR MELATI</span>
+      </nav>
+      <div>
+
+      </div>
+    </>
+  );
 }
 
 export default Question;
