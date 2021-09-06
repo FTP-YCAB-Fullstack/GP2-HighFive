c1a8521f755f1b832cfca308186f72e7e24102c0
AlimMakruf <alimm.makruf@gmail.com>
2021-09-06 14:14:50 +0700

    update

--- HighTrivia/src/components/Result.jsx
+++ HighTrivia/src/components/Result.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 
-function Result () {
+function ResultPage (props) {
+
     return (
         <>
-        {props.location.state && result.answers.length > 0 ? (
-        <>
-        <div className="flex flex-col justify-center items-center w-2/3 h-2/3 py-12 bg-blue-400 rounded-2xl">
-          <div className="text-2xl">Selamat Anda Mendapatkan Nilai</div>
-          <div className="flex flex-wrap h-auto w-4/5 mt-8 justify-center items-center">
+        <div className="bg-blue-500 w-screen h-screen flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center w-2/3 h-2/3 py-12 bg-blue-400 rounded-2xl">
+            <div className="text-2xl">Selamat Anda Mendapatkan Nilai</div>
+            <div className="flex flex-wrap h-auto w-4/5 mt-8 justify-center items-center">
+            {props.location.state && props.result.answers.length > 0 ? (
+                <React.Fragment>
+                <div>
+                  <h2>{props.result.score}</h2>
+                </div>
+                </React.Fragment>
+              ) : (
+                <h1>Tolong lakukan Quiz terlebih dahulu</h1>
+              )}
+              <button onClick={props.onClickBackToHome}>Back To Home</button>
+              {/* <button className="bg-white w-1/3 m-5 rounded-md px-7 py-3">Jawaban A</button> */}
+            </div>
           </div>
         </div>
-        
-
-          <h1>Selamat Anda Mendapatkan Nilai</h1>
-          <h2>{result.score}</h2>
-        </>
-      ) : (
-        <h1>Tolong lakukan Quiz terlebih dahulu</h1>
-      )}
-      <button onClick={onClickBackToHome}>Back To Home</button>
         </>
     )
 }
 
-export default Result
+export default ResultPage
--- HighTrivia/src/pages/Result.js
+++ HighTrivia/src/pages/Result.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { resetResult } from "../redux/action";
+import ResultPage from "../components/Result";
 
 function Result(props) {
   const result = useSelector(state => state.result);
@@ -21,7 +22,10 @@ function Result(props) {
 
   return (
     <>
-      {props.location.state && result.answers.length > 0 ? (
+      <ResultPage onClickBackToHome={onClickBackToHome} result={result} {...props}/>
+
+
+      {/* {props.location.state && result.answers.length > 0 ? (
         <>
         <div>
           <h1>Selamat Anda Mendapatkan Nilai</h1>
@@ -31,7 +35,7 @@ function Result(props) {
       ) : (
         <h1>Tolong lakukan Quiz terlebih dahulu</h1>
       )}
-      <button onClick={onClickBackToHome}>Back To Home</button>
+      <button onClick={onClickBackToHome}>Back To Home</button> */}
     </>
   );
 }
