47bef5a3f280851bb5d0b2dd0b7c6331b50e702c
AlimMakruf <alimm.makruf@gmail.com>
2021-09-07 16:38:14 +0700

    update

--- HighTrivia/src/pages/Home.js
+++ HighTrivia/src/pages/Home.js
@@ -33,7 +33,7 @@ function Home(props) {
   return (
     <>
       {/* <Homepage /> */}
-      <nav className="flex justify-between bg-gray-400 p-3 absolute top-0 z-50 w-full">
+      <nav className="flex justify-between bg-blue-400 p-3 absolute top-0 z-50 w-full">
         <span className="text-white">Logo</span>
         <button
           className="bg-white px-3 rounded-md"
@@ -45,17 +45,21 @@ function Home(props) {
 
       <div className="flex h-screen justify-center items-center">
         <div className="flex justify-center items-center flex-col justify-around h-2/5 w-1/2">
-        <select className="flex bg-gray-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
-            <option value="0">Anime</option>
-            <option value="1">Matematika</option>
-            <option value="2">Games</option>
-        </select>
-        <select className="flex bg-gray-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
-            <option value="0">Easy</option>
-            <option value="1">Medium</option>
-            <option value="2">Hard</option>
-        </select>
-        <button className="bg-gray-400 py-4 px-8 rounded-lg text-white" onClick={onClickStart}>Start</button>
+        <div className="select-box">
+          <select className="flex bg-blue-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
+              <option value="0">Anime</option>
+              <option value="1">Matematika</option>
+              <option value="2">Games</option>
+          </select>
+        </div>
+        <div className="select-box">
+          <select className="flex bg-blue-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
+              <option value="0">Easy</option>
+              <option value="1">Medium</option>
+              <option value="2">Hard</option>
+          </select>
+        </div>
+        <button className="bg-blue-500 py-4 px-10 rounded-lg text-white" onClick={onClickStart}>Start</button>
           </div>
       </div>
 
