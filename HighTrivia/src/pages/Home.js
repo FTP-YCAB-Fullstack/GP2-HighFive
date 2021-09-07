fb7e4b90f78fd2fb283fa3313b04d1975ff0a3de
AlimMakruf <alimm.makruf@gmail.com>
2021-09-07 18:28:16 +0700

    resolve conflict

--- HighTrivia/src/pages/Home.js
+++ HighTrivia/src/pages/Home.js
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
 import access from "../utils/access";
-<<<<<<< HEAD
-import { useDispatch } from "react-redux";
-import { setViaHome, resetResult } from "../redux/action";
-import '../css/HomePage.css'
-=======
 import { useDispatch, useSelector } from "react-redux";
-import { setViaHome, resetResult, setQuiz, login } from "../redux/action";
-// import Homepage from "../components/HomePage";
->>>>>>> cf415e6e3c8bb73ff2c38925ccec9f27d920bef6
+import { setViaHome, resetResult, setQuiz, login} from "../redux/action";
+import '../css/HomePage.css'
 
 function Home(props) {
   const isLogin = useSelector(state => state.access.isLogin);
@@ -47,12 +41,8 @@ function Home(props) {
 
   return isLogin === null ? (<h1>waiting</h1>) : (
     <>
-<<<<<<< HEAD
       {/* <Homepage /> */}
       <nav className="flex justify-between bg-blue-400 p-3 absolute top-0 z-50 w-full">
-=======
-      <nav className="flex justify-between bg-gray-400 p-3 absolute top-0 z-50 w-full">
->>>>>>> cf415e6e3c8bb73ff2c38925ccec9f27d920bef6
         <span className="text-white">Logo</span>
         {!isLogin ? (
           <button
@@ -72,16 +62,19 @@ function Home(props) {
 
       <div className="flex h-screen justify-center items-center">
         <div className="flex justify-center items-center flex-col justify-around h-2/5 w-1/2">
-<<<<<<< HEAD
         <div className="select-box">
-          <select className="flex bg-blue-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
+          <select className="flex bg-blue-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white"
+          onChange={event => setCategory(event.target.value)}
+          >
               <option value="0">Anime</option>
               <option value="1">Matematika</option>
               <option value="2">Games</option>
           </select>
         </div>
         <div className="select-box">
-          <select className="flex bg-blue-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
+          <select className="flex bg-blue-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white"
+          onChange={event => setDifficult(event.target.value)}
+          >
               <option value="0">Easy</option>
               <option value="1">Medium</option>
               <option value="2">Hard</option>
@@ -89,51 +82,8 @@ function Home(props) {
         </div>
         <button className="bg-blue-500 py-4 px-10 rounded-lg text-white" onClick={onClickStart}>Start</button>
           </div>
-=======
-          <select
-            className="flex bg-gray-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white"
-            onChange={event => setCategory(event.target.value)}
-          >
-            <option value="31">Anime</option>
-            <option value="19">Matematika</option>
-            <option value="15">Games</option>
-          </select>
-          <select
-            className="flex bg-gray-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white"
-            onChange={event => setDifficult(event.target.value)}
-          >
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-          </select>
-          <button
-            className="bg-gray-400 py-4 px-8 rounded-lg text-white"
-            onClick={onClickStart}
-          >
-            Start
-          </button>
-        </div>
->>>>>>> cf415e6e3c8bb73ff2c38925ccec9f27d920bef6
       </div>
 
-      {/* <div className="flex h-screen justify-center items-center">
-        <div className="flex justify-center items-center flex-col justify-around h-2/5 w-1/2">
-          <div className="flex bg-gray-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
-            <span>Category</span>
-            <span>v</span>
-          </div>
-          <div className="flex bg-gray-400 w-2/3 justify-between py-4 px-5 rounded-lg text-white">
-            <span>Difficulty</span>
-            <span>v</span>
-          </div>
-          <button
-            className="bg-gray-400 py-4 px-8 rounded-lg text-white"
-            onClick={onClickStart}
-          >
-            Start
-          </button>
-        </div>
-      </div> */}
     </>
   );
 }
--- HighTrivia/src/pages/Result.js
+++ HighTrivia/src/pages/Result.js
@@ -1,11 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { resetResult } from "../redux/action";
-<<<<<<< HEAD
-import ResultPage from "../components/Result";
-=======
 import ResultPage from "../components/ResultPage";
->>>>>>> cf415e6e3c8bb73ff2c38925ccec9f27d920bef6
 
 function Result(props) {
   const result = useSelector(state => state.result);
@@ -26,28 +22,11 @@ function Result(props) {
 
   return (
     <>
-<<<<<<< HEAD
-      <ResultPage onClickBackToHome={onClickBackToHome} result={result} {...props}/>
-
-
-      {/* {props.location.state && result.answers.length > 0 ? (
-        <>
-        <div>
-          <h1>Selamat Anda Mendapatkan Nilai</h1>
-          <h2>{result.score}</h2>
-        </div>
-        </>
-      ) : (
-        <h1>Tolong lakukan Quiz terlebih dahulu</h1>
-      )}
-      <button onClick={onClickBackToHome}>Back To Home</button> */}
-=======
       <ResultPage
         onClickBackToHome={onClickBackToHome}
         result={result}
         {...props}
       />
->>>>>>> cf415e6e3c8bb73ff2c38925ccec9f27d920bef6
     </>
   );
 }
